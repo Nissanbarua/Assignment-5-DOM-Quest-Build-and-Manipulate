6fbfecc90fced9561e393a91bedf41b9eb8f9d24
Nissan Barua <Baruanissan55@gmail.com>
2024-09-25 13:21:56 +0600

    file redrecting checking

---
+++ js/donatebd.js
@@ -0,0 +1,8 @@
+document.getElementById("blog-btn").addEventListener("click", function () {
+  console.log("blog btn clicked");
+  window.location.href = "../blog.html";
+});
+document.getElementById("history-tab").addEventListener("click", function () {
+  console.log("history btn clicked");
+  window.location.href = "../history.html";
+});
